c0a369b956af2260f9e54fd1396d438b6ccccc46
Allan Weber <a.cassianoweber@gmail.com>
2020-05-15 13:19:06 +0200

    Add Basic Authentication

---
+++ api/src/main/java/com/allanweber/api/configuration/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.allanweber.api.configuration;
+
+import com.allanweber.api.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	private final UserService userService;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.authorizeRequests().antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/admin/contacts/**", "/users/**").hasRole("ADMIN")
+				.anyRequest().hasRole("USER")
+				.and()
+                .httpBasic();
+
+		httpSecurity.csrf().disable();
+		httpSecurity.headers().frameOptions().disable();
+    }
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}

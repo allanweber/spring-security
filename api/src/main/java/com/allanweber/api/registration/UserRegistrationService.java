a22021b3228bc7bd34b3f7ab1fa5f3e2c7490bd6
Allan Weber <a.cassianoweber@gmail.com>
2020-05-17 15:09:46 +0200

    Add email verification feature flag

--- api/src/main/java/com/allanweber/api/registration/UserRegistrationService.java
+++ api/src/main/java/com/allanweber/api/registration/UserRegistrationService.java
@@ -6,6 +6,7 @@ import com.allanweber.api.user.UserDto;
 import com.allanweber.api.user.UserEntity;
 import com.allanweber.api.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,12 +20,20 @@ public class UserRegistrationService {
     private final ApplicationEventPublisher eventPublisher;
     private final VerificationService verificationService;
 
+    @Value("${application.email-verification-enabled}")
+    private Boolean emailVerificationEnabled;
+
     public UserDto register(UserRegistration user) {
         String password = encoder.encode(user.getPassword());
         UserEntity userEntity = UserEntity.create(user.getUserName(),user.getFirstName(),user.getLastName(), password, user.getEmail());
         UserDto userDto = userService.createUser(userEntity);
 
-        eventPublisher.publishEvent(new UserRegistrationEvent(userDto));
+        if(emailVerificationEnabled) {
+            eventPublisher.publishEvent(new UserRegistrationEvent(userDto));
+        }
+        else {
+            userService.setUserVerified(userDto.getUsername());
+        }
 
         return userDto;
     }
